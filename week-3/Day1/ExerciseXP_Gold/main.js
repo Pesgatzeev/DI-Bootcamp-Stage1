17708cededcbd4332485918dd7394efc75e121db
Kapko Maksym <kapko_maksym@i.ua>
2023-01-22 18:03:10 +0200

    Daily challenge added

---
+++ week-3/Day1/ExerciseXP_Gold/main.js
@@ -0,0 +1,42 @@
+// Exercise 1 : Favorite Color
+// Instructions
+// let sentence = ["my","favorite","color","is","blue"];
+// Write some simple Javascript code that will join all the items in the array above, and console.log the result.
+
+// let sentence = ["my","favorite","color","is","blue"];
+// console.log(sentence.join(' '));
+
+// Exercise 2 : Mixup
+// Instructions
+// Create 2 variables. The values should be strings. For example:
+// let str1 = "mix";
+// let str2 = "pod";
+
+
+// 2. Slice out and swap the first 2 characters of the two strings from part 1.
+
+
+// 3. Create a third variable where the value is the concatenation of the two strings from the part 1 (separated by a space).
+
+
+// 4. Finally console.log the new concatenated string.
+
+
+
+// Some Examples
+
+// let str1 = "mix";
+// let str2 = "pod";
+// let newWord should be equal to 'pox mid'
+
+// let firstWord = "Hello";
+// let secondWord = "World";
+// let thirdWord should be equal to 'Wollo Herld'
+
+
+let firstStr = 'train';
+let secondStr = 'transiberian';
+
+firstStr.slice(0,2);
+console.log(firstStr);
+
