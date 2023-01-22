2b1c39bd9758caec8f6dd7bdd300226d91d1788c
Kapko Maksym <kapko_maksym@i.ua>
2023-01-22 19:30:18 +0200

    xp gold done

--- week-3/Day1/ExerciseXP_Gold/main.js
+++ week-3/Day1/ExerciseXP_Gold/main.js
@@ -34,9 +34,48 @@
 // let thirdWord should be equal to 'Wollo Herld'
 
 
-let firstStr = 'train';
-let secondStr = 'transiberian';
+//let firstStr = 'train';
+//let secondStr = 'kransiberian';
 
-firstStr.slice(0,2);
-console.log(firstStr);
+//firstStr = 'kr' + firstStr.slice(2);
+//console.log(firstStr);
 
+//secondStr = 'tr' + secondStr.slice(2);
+// console.log(secondStr);
+
+//let thirdStr = `${firstStr} ${secondStr}`;
+//console.log(thirdStr);
+
+
+// Exercise 3 : Calculator
+// Instructions
+// Make a Calculator. Follow the instructions:
+
+// Prompt the user for the first number.
+// Store the first number in a variable called num1.
+// Hint : console.log the type of the variable num1. What should you do to convert it to a number ?
+// Prompt the user for the second number.
+// Store the second number in a variable called num2.
+// Create an Alert where the value is the SUM of num1 and num2.
+// BONUS: Make a program that can subtract, multiply, and also divide!
+
+
+//  let num1 = + prompt ('Pls, enter first number');
+//  let num2 =  + prompt('Pls, enter second number');
+//  let sum = num1 + num2;
+//  alert(sum);
+
+//  let num1 = + prompt ('Pls, enter first number');
+//  let num2 =  + prompt('Pls, enter second number');
+//  let sum = num1 * num2;
+//  alert(sum);
+
+//  let num1 = + prompt ('Pls, enter first number');
+//  let num2 =  + prompt('Pls, enter second number');
+//  let sum = num1 - num2;
+//  alert(sum);
+
+//  let num1 = + prompt ('Pls, enter first number');
+//  let num2 =  + prompt('Pls, enter second number');
+//  let sum = num1 / num2;
+//  alert(sum);
