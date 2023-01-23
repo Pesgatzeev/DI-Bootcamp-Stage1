017cbba492ac965a3f46b36d06196d4ccf8f98d8
Kapko Maksym <kapko_maksym@i.ua>
2023-01-23 16:28:24 +0200

    XP exercise done

---
+++ week-3/Day1/Exercise XP_Ninja/script.js
@@ -0,0 +1,40 @@
+// Exercise 1 : Evaluation
+// Instructions
+// For each expression, predict what you think the output will be in a comment (//) without first running the command.
+// Of course, explain each prediction.
+// Then run the expression in the console. Note the actual output in a comment and compare it with your prediction.
+
+// Evaluate the comparisons found below:
+
+// Look at this link for help
+
+//     // 5 >= 1 
+//     // true
+//     // 0 === 1
+//     // false
+    // 4 <= 1
+    // true
+    // 1 != 1
+    // false
+    // "A" > "B"
+    // false
+    // "B" < "C"
+    // true
+    // "a" > "A"
+    // true
+    // "b" < "A"
+    // false
+    // true === false
+    // false
+    // true != true
+    // false
+
+
+    // let userQuestion = prompt ("Pls enter list of numbers devided by coma,");
+    // userQuestion.split;
+    // let sum =userQuestion.replace(/,/g, '+');
+
+    // console.log(sum);
+
+    const user = prompt("I love the movie named Nemo);
+    console.log(user.indexOf('Nemo'));
