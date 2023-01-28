31fd9de252f8e9153bbc829a0d62a4c19a61d18c
Kapko Maksym <kapko_maksym@i.ua>
2023-01-28 13:47:17 +0200

    Daily chalenge

---
+++ week-3/Day4/Daily-challenge/script.js
@@ -0,0 +1,26 @@
+/ Put the words into an array.
+// Hello,hi,morning
+// Console.log the words one per line, in a rectangular frame as seen below.
+// Check out the Hints and Requirements below.
+let sentence = prompt(`Please, enter several words (separated by commas and space)`);
+sentence = sentence.split(“, “);
+console.log(sentence);
+let longest = 0;
+// let arr = [“Hello”, “myfriend”, “by”]
+for (let word of sentence) {
+ if (word.length>longest){
+    longest = word.length;
+ }
+}
+let div;
+for (let i = -1; i <= sentence.length; i++){
+    if (i<0 || i === sentence.length){
+        div = “”;
+        div = div.padEnd(longest+4,“*”)
+        console.log(div)
+    } else  {
+        div = “”;
+        div = “* “+sentence[i] + div.padEnd(longest-sentence[i].length,” “) + ” *“;
+        console.log(div);
+    }
+}
